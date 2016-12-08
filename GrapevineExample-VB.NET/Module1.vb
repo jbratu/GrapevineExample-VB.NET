dd26e7da71fc8de5a3186dd040f5ec58a9a277c6
Jared Bratu <jared.bratu@ncbi.com>
2016-12-08 15:54:58 -0500

    Basic Grapevine VB.NET usage example.

---
+++ GrapevineExample-VB.NET/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports Grapevine.Server
+Imports Grapevine.Server.Attributes
+Imports Grapevine.Interfaces.Server
+Imports ConsoleApplication1.TestResource
+
+Module Module1
+
+    Sub Main()
+        Dim server As New RestServer()
+
+        server.LogToConsole().Start()
+        Console.ReadLine()
+        server.Stop()
+
+    End Sub
+
+End Module
---
+++ GrapevineExample-VB.NET/TestResource.vb
@@ -0,0 +1,14 @@
+﻿Imports Grapevine.Server
+Imports Grapevine.Server.Attributes
+Imports Grapevine.Interfaces.Server
+
+<RestResource>
+Public Class TestResource
+
+    <RestRoute>
+    Public Function HelloWorld(ByVal content As IHttpContext) As IHttpContext
+        content.Response.SendResponse("Hello World")
+        Return content
+    End Function
+
+End Class
